f204dfbae98a63b721c9a32c8300e90203244b48
Yile Liu <yile.liu@autodesk.com>
2017-02-23 11:20:34 +0800

    change provision

--- provision.sh
+++ provision.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 apt-get -y update
+apt-get install curl
 
 dpkg -s npm &> /dev/null || {
-	apt-get -y install nodejs npm
+	curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+	sudo apt-get install -y nodejs
 	ln -s /usr/bin/nodejs /usr/bin/node
 }
 
 command -v hubot &>/dev/nul || {
-	npm install -g hubot coffee-script
+	 npm install -g yo generator-hubot coffee-script
+	 mkdir -p /vagrant/myhubot
+	 cd /vagrant/myhubot
 }
